7aa67001c002d5ee8d7085a846043746456aa6fb
Hridoy Saha <hridoysaha969@gmail.com>
2024-11-20 22:47:17 +0600

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Enable or disable strict mode as needed
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignore ESLint errors during the build
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during the build
   },
 };
 
